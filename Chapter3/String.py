8a70d665d02f427e53ee031ebac0973296e8925a
Krishna Garg <garg79408@gmail.com>
2026-08-26 19:53:56 +0530

    snwd

--- Chapter3/String.py
+++ Chapter3/String.py
@@ -37,3 +37,32 @@ print(word.strip().lower().replace("world","Krishna").title())
 
 a = "Krishna is a \'good\' boy "
 print(a)
+
+
+
+names = "KrisHna garg"
+print(names.lower())
+print(names.upper())
+print(names.capitalize())
+print(names.title())
+
+names1 = "python sql msexcel powerbi"
+print(names1.split()) # convert Sting---->List
+namess1 = "python,sql,msexcel,powerbi"
+print(namess1.split(","))
+
+names2 = ["Python" , "SQL" , "MsExcel" ,"Powerbi"]
+print(",".join(names2)) # convert List---->String
+names3 = ["Data" , "Analyst"]
+print(" ".join(names3))
+
+text = "banana"
+print(text.count("a"))
+
+email = "Krishna1705@gmail.com"
+print(email.startswith("Krishna"))
+print(email.endswith("@gmail.com"))
+
+name = "Krishna"
+age = 22
+print(f"Hello {name} your age is {age}")
